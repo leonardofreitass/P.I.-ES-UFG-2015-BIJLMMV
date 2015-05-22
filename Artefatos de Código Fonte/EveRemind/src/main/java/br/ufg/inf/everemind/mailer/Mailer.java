53c361a47fcf3dafe1895733a7e6f3a66de43c4b
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2015-05-22 03:38:40 -0300

    Implementando a classe Mailer e parcialmente a classe EmailAgent

---
+++ Artefatos de Código Fonte/EveRemind/src/main/java/br/ufg/inf/everemind/mailer/Mailer.java
@@ -0,0 +1,97 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Leonardo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.ufg.inf.everemind.mailer;
+
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Leonardo
+ */
+class Mailer implements Runnable {
+    
+    private final String SMPT_HOST = "smtp.zoho.com";
+    private final String SMTP_PORT = "465";
+    private final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+    private final String SENDER_EMAIL = "no-reply@everemind.herokuapp.com";
+    private final String SENDER_PASSWORD = "evestaff321";
+    private final String receiver;
+    private final String subject;
+    private final String content;
+
+    public Mailer(String receiver, String subject, String content) {
+        this.receiver = receiver;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    @Override
+    public void run() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", SMPT_HOST);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.port", SMTP_PORT);
+        props.put("mail.smtp.socketFactory.port", SMTP_PORT);
+        props.put("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.put("mail.smtp.socketFactory.fallback", "false");
+
+        Session session;
+        session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(SENDER_EMAIL, SENDER_PASSWORD);
+                    }
+                });
+
+        session.setDebug(true);
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(SENDER_EMAIL));
+
+            Address[] toUser = InternetAddress
+                    .parse(receiver);
+
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
